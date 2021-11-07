95a67fea821416365d319fbb68c78553ffca3412
Saiqul Haq <saiqulhaq@gmail.com>
2021-11-07 15:27:39 +0700

    Init project

---
+++ lib/hello/workflow.rb
@@ -0,0 +1,8 @@
+require "hello/workflow/version"
+
+module Hello
+  module Workflow
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ spec/hello/workflow_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Hello::Workflow do
+  it "has a version number" do
+    expect(Hello::Workflow::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
